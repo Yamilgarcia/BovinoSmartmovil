c139ada599a40ffc8c334f08e5edc54d643c3ea1
Daniela Baltodano <alejandrabaltodano67@gmail.com>
2024-11-21 08:27:39 -0600

    ultimo xd

--- __mocks__/firebase.js
+++ __mocks__/firebase.js
@@ -2,21 +2,21 @@
 export const initializeApp = jest.fn(() => ({}));
 
 // Simula Firebase Firestore
-export const getFirestore = jest.fn(() => ({
-  collection: jest.fn(() => ({
-    addDoc: jest.fn(() => Promise.resolve({ id: 'mock-id' })), // Simula addDoc
-    getDocs: jest.fn(() =>
-      Promise.resolve({
-        docs: [
-          {
-            id: 'mock-id',
-            data: () => ({ codigo_idVaca: '12345', nombre: 'Vaca Test' }),
-          },
-        ],
-      })
-    ), // Simula getDocs
-  })),
-}));
+export const getFirestore = jest.fn(() => ({}));
+
+// Mock global para Firestore
+export const collection = jest.fn(() => ({}));
+export const addDoc = jest.fn(() => Promise.resolve({ id: 'mock-id' })); // Simula addDoc
+export const getDocs = jest.fn(() =>
+  Promise.resolve({
+    docs: [
+      {
+        id: 'mock-id',
+        data: () => ({ codigo_idVaca: '12345', nombre: 'Vaca Test' }),
+      },
+    ],
+  })
+);
 
 // Simula Firebase Auth
 export const initializeAuth = jest.fn(() => ({
